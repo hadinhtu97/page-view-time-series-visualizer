1a6aaf4339c271f1c44adb4e6535214ad4a578c8
Tu Ha Dinh <hadinhtu97@gmail.com>
2021-01-26 23:39:58 +0700

    update: draw line plot

--- module/time_series_visualizer.py
+++ module/time_series_visualizer.py
@@ -2,9 +2,17 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
+df = pd.read_csv('./data/data.csv', index_col='date', parse_dates=True)
+df = df[(df['value'] >= df['value'].quantile(0.025))
+        & (df['value'] <= df['value'].quantile(0.975))]
+
 
 def draw_line_plot():
-    pass
+    plt.plot(df.index, df['value'], c='r')
+    plt.xlabel('Date')
+    plt.ylabel('Page Views')
+    plt.title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
+    plt.savefig('./img/line_plot.png')
 
 
 def draw_bar_plot():
